341e5d17871104d0e59e5253382631bf91d227dc
黄格洋 <v-huanggeyang@shuwen.com>
2021-08-30 23:40:42 +0800

    HelloWorld&注释&关键字&保留字

--- README.md
+++ README.md
@@ -3,3 +3,6 @@ https://www.bilibili.com/video/BV1Kb411W75N?p=16
 
 8-29 jdk jre jvm关系&环境搭建
 https://www.bilibili.com/video/BV1Kb411W75N?p=32
+
+8-30HelloWorld&注释&关键字&保留字
+https://www.bilibili.com/video/BV1Kb411W75N?p=46&spm_id_from=pageDriver
---
+++ java_note/src/main/java/day01/zhushi.java
@@ -0,0 +1,31 @@
+package day01;
+//单行注释
+
+/*
+多行注释  注意不能嵌套使用
+注释：对代码做解释说明
+ */
+
+/**
+ * 文档注释
+ *API  Application Programming Interface，应用程序接口
+ */
+
+/*
+ 关键字都是小写
+ 保留字： goto const 避开命名
+ java中可以有多个class 但只能有一个public class(public 只能加在和文件名一样的class上)
+ */
+
+public class zhushi {
+
+    public static void main(String[] s){
+//        public static void main(String s[])
+        System.out.println("hello World---");
+//      ln换行
+        System.out.print("hello World---");
+    }
+}
+
+
+
